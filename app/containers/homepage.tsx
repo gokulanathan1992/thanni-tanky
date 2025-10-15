f274a3611892596ffbaa7bdf95b693d2bce03707
gokulanathan1992 <gokulanathan1992@gmail.com>
2025-10-15 14:17:57 +0000

    Created Homepage container with minimal text and set the background colour to grey.

---
+++ app/containers/homepage.tsx
@@ -0,0 +1,20 @@
+import { Text, View } from "react-native";
+// import { Button } from 'react-native-paper';
+
+const Homepage = () => {
+    return (
+        <View
+            style={{
+                backgroundColor: "grey",
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center",
+                width: '100%',
+            }}
+        >
+            <Text>Vanakkam!</Text>
+        </View>
+    );
+};
+
+export default Homepage;
--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,5 @@
-import { Text, View } from "react-native";
+import { View } from "react-native";
+import Homepage from "./containers/homepage";
 
 export default function Index() {
   return (
@@ -9,7 +10,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Vanakkam!</Text>
+      <Homepage />
     </View>
   );
 }
